c3c4b89a17f0464a7f015ca49e948cebf913b373
Edwintang2005 <91371446+Edwintang2005@users.noreply.github.com>
2022-12-03 13:52:42 +1100

    Initial Commit

---
+++ Gifted/GiftedApp.swift
@@ -0,0 +1,17 @@
+//
+//  GiftedApp.swift
+//  Gifted
+//
+//  Created by Edwin Tang on 3/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct GiftedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
