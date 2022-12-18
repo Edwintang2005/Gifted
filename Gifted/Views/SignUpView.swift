d2f6e6f41975757a8f97f2cff542dad90a6b00d9
Edwintang2005 <91371446+Edwintang2005@users.noreply.github.com>
2022-12-18 22:42:17 +1100

    Updated all signup/signin functionality including value check

--- Gifted/Views/SignUpView.swift
+++ Gifted/Views/SignUpView.swift
@@ -17,21 +17,42 @@ struct SignUpView: View {
     @State var email = ""
     @State var password = ""
     
+    private var requirements: Array<Bool> {
+        [
+            password.count >= 8,  // Verifying Minimum Length
+            isValidEmailAddress(emailAddressString: email)
+        ]
+    }
+    
     var body: some View {
         VStack {
-            Text("Sign Up Here:")
-                .font(.title3)
             Spacer()
             TextField("Username", text: $username).pretty()
             TextField("Email", text: $email).pretty()
-            SecureField("Password (at least 8 characters)", text: $password).pretty()
-            Button("Sign Up", action: {
+            SecureField("Password", text: $password).pretty()
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("Signup Conditions:").homepagename()
+                    Text("•  Valid Email Address")
+                        .verif()
+                        .foregroundColor(self.requirements[1] ? .green: .red)
+                    Text("•  Password longer than 8 characters")
+                        .verif()
+                        .foregroundColor(self.requirements[0] ? .green : .red)
+                }
+                Spacer()
+            }
+            
+            Button {
                 sessionManager.signUp(
                     username: username,
                     email: email,
                     password: password
                 )
-            }).pretty()
+            } label: {
+                Text("Sign up!")
+                    .frame(maxWidth: .infinity)
+            }.pretty()
             Spacer()
             Button("Already have an account? Log in.", action: sessionManager.showLogin)
             Spacer()
@@ -40,6 +61,28 @@ struct SignUpView: View {
         .padding()
     }
     
+    func isValidEmailAddress(emailAddressString: String) -> Bool {
+      
+      var returnValue = true
+      let emailRegEx = "[A-Z0-9a-z.-_]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,3}"
+      
+      do {
+          let regex = try NSRegularExpression(pattern: emailRegEx)
+          let nsString = emailAddressString as NSString
+          let results = regex.matches(in: emailAddressString, range: NSRange(location: 0, length: nsString.length))
+          
+          if results.count == 0
+          {
+              returnValue = false
+          }
+          
+      } catch let error as NSError {
+          print("invalid regex: \(error.localizedDescription)")
+          returnValue = false
+      }
+      
+      return  returnValue
+  }
 }
 
 
