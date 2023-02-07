e7a6541336beffbf39287c4a83ab53b92e6c9951
Edwintang2005 <91371446+Edwintang2005@users.noreply.github.com>
2023-02-08 10:34:24 +1100

    Attempt at resolving list push to cloud bug (failed)

--- amplify/generated/models/AmplifyModels.swift
+++ amplify/generated/models/AmplifyModels.swift
@@ -8,9 +8,9 @@ final public class AmplifyModels: AmplifyModelRegistration {
   public let version: String = "87f8e0fa78801decf075b94f878cb3d7"
   
   public func registerModels(registry: ModelRegistry.Type) {
+    ModelRegistry.register(modelType: UserList.self)
     ModelRegistry.register(modelType: ListItem.self)
     ModelRegistry.register(modelType: UserProfile.self)
-    ModelRegistry.register(modelType: UserList.self)
     ModelRegistry.register(modelType: Group.self)
     ModelRegistry.register(modelType: Reservations.self)
   }
